470b31bbdc569075e7ef9a812a8e76d1651609e4
apfols <apfols@gmail.com>
2020-03-21 11:06:20 +0800

    Add swap node solution

---
+++ src/interview1/SwapNode.java
@@ -0,0 +1,53 @@
+package interview1;
+
+
+public class SwapNode {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+    /**
+     * Solution 1: Use recursive
+     * time: O(n)
+     */
+
+//    public ListNode swapPairs(ListNode head) {
+//        if (head == null || head.next == null)
+//            return head;
+//
+//        ListNode result = head.next;
+//        head.next = swapPairs(head.next.next);
+//        result.next = head;
+//        return result;
+//    }
+
+
+    /**
+     * Solution 2: Iterate over each node
+     * time: O(n)
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode result = head.next;
+        head.next = result.next;
+        result.next = head;
+
+        ListNode ptr = result.next;
+
+        while (ptr.next != null && ptr.next.next != null) {
+            ListNode temp = ptr.next;
+            ptr.next = temp.next;
+            temp.next = ptr.next.next;
+            ptr.next.next = temp;
+
+            ptr = ptr.next.next;
+        }
+
+
+        return result;
+    }
+}
